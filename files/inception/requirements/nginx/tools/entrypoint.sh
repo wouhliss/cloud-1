1f70a5d8ad2422f39cab9c41800f94744373f679
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2025-02-25 12:12:37 +0100

    feat: too much things

--- files/inception/requirements/nginx/tools/entrypoint.sh
+++ files/inception/requirements/nginx/tools/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ ! -f ~/ssl ]; then
+if [ ! -f /etc/letsencrypt/has_tls ]; then
 	sed -i -e "s/%DOMAIN_NAME%/$DOMAIN_NAME/g" /etc/nginx/sites-enabled/wordpress.conf
 
 	nginx
 
 	certbot run -n --agree-tos -d $DOMAIN_NAME -m wouhliss@student.42.fr --redirect --nginx
 
-	touch ~/ssl
+	touch /etc/letsencrypt/has_tls
 
 	nginx -s stop
 fi
